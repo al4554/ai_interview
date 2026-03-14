0ba4a1251c212979ef2638e4f49eff7fd522047f
al4554 <alok4596p@gmail.com>
2026-03-14 22:52:58 +0530

    added cors for frontend

--- server/src/server.js
+++ server/src/server.js
@@ -14,12 +14,20 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || "http://localhost:5173",
+//     credentials: true
+//   })
+// );
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://aiinterview-kappa.vercel.app"
+  ],
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
